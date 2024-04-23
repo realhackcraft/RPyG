c3858d991ea7bdf9dc2cdf35b2438f6ab9ac5c44
Hackcraft_ <hackcraft_@hotmail.com>
2024-04-23 15:21:11 -0400

    Fixed the issue
    Signed-off-by: Hackcraft_ <hackcraft_@hotmail.com>

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import re
 from classes.bear import Bear
 from classes.entity import Entity
 from classes.player import Player
-from classes.map import Map
+from classes.map import Map, deepcopy
 from utils.displayManager import DisplayManager
 from utils.string import StringUtils
 
@@ -37,26 +37,21 @@ def clear_display():
 
 def main():
   player = Player(2, 3)
-  entities = []
   game_map = Map("./assets/map.txt", display_manager)
   last_user_input = ""
   
   ### TEST ###
-  entities.append(Bear(10, 10))
+  game_map.entities.append(Bear(10, 10))
   ### END TEST ###
 
   print("\033[=3h")  # Set display mode to 80x25 with color support
 
   while True:
     clear_display()
-    game_map.render_map(player, entities, last_user_input)
+    game_map.render_map(player, game_map.entities, last_user_input)
     display_manager.flush_buffer()
 
-    for e in entities:
-      if type(e) is Bear:
-        e.tick(player, game_map)
-      else:
-        e.tick()
+
 
     user_input = get_input()
 
@@ -68,33 +63,55 @@ def main():
     commands = StringUtils.split_string_with_capitals(user_input)
 
     for c in commands:
+      for e in game_map.entities:
+        if type(e) is Bear:
+          e.tick(player, game_map)
+        else:
+          e.tick()
+
       match c.lower():
         case "w":
           if player.y > 0:
             player.y -= 1
-          elif game_map.up:
-            game_map = Map(game_map.up, display_manager) # Load upper map
+          elif game_map.loaded_up:
+            temp_game_map = game_map
+            game_map = game_map.loaded_up # Load upper map
+            game_map.load_path() # Only load the surrounding maps once it is loaded
+            game_map.loaded_down = temp_game_map
+
             player.y = len(game_map) - 1
             player.x = min(len(game_map[player.y]) - 1, player.x) # Make sure player.x is in bounds
         case "s":
-          if player.y + 1 < len(game_map) :
+          if player.y + 1 < len(game_map):
             player.y += 1
-          elif game_map.down:
-            game_map = Map(game_map.down, display_manager)
+          elif game_map.loaded_down:
+            temp_game_map = game_map
+            game_map = game_map.loaded_down
+            game_map.load_path()
+            game_map.loaded_up = temp_game_map
+
             player.y = 0
             player.x = min(len(game_map[player.y]) - 1, player.x) # Make sure player.x is in bounds
         case "a":
           if player.x > 0:
             player.x -= 1
           elif game_map.left:
-            game_map = Map(game_map.left, display_manager)
+            temp_game_map = game_map
+            game_map = game_map.loaded_left
+            game_map.load_path()
+            game_map.loaded_right = temp_game_map
+
             player.x = 0
             player.y = min(len(game_map) - 1, player.y) # Make sure player.y is in bouds
         case "d":
           if player.x + 1 < len(game_map[player.y]):
             player.x += 1
           elif game_map.right:
-            game_map = Map(game_map.right, display_manager)
+            temp_game_map = game_map
+            game_map = game_map.loaded_right
+            game_map.load_path()
+            game_map.loaded_left = temp_game_map
+
             player.x = len(game_map[player.y]) - 1
             player.y = min(len(game_map) - 1, player.y) # Make sure player.y is in bouds
         case "m":
@@ -109,16 +126,16 @@ def main():
               game_map[player.y][player.x] = "R"
               if random.getrandbits(2) == 0:
                 e = Entity("Salmon", "S", player.x, player.y, 1)
-                entities.append(e)
+                game_map.entities.append(e)
         case "e":
-          if len(entities) > 0:
-            for e in entities:
+          if len(game_map.entities) > 0:
+            for e in game_map.entities:
               if e.x == player.x and e.y == player.y:
                 e.health -= 1
                 if e.health <= 0:
                   if e.name == "Salmon":
                     if player.set_hunger(player.hunger + 1):
-                      entities.remove(e)
+                      game_map.entities.remove(e)
 
 if __name__ == "__main__":
   main()
